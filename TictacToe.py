cdc43d2374970ed8fed9d27c69ae0223a8bf1aff
sowrabhmumadi <sowrabhmummadi@gmail.com>
2019-08-30 14:53:32 +0200

    TicTacToe: final commit for version 1.0    - added conditions to end game    - added user input handling    - added draw condition    - Refactoring    - good looking user messages

--- TictacToe.py
+++ TictacToe.py
@@ -7,9 +7,8 @@ selected_position_list = []
 
 def print_board():
     for index, row in enumerate(board):
-        # print(f"-----|-----|-----|")
         print(f"  {board[index][0]}  |  {board[index][1]}  |  {board[index][2]}  ")
-        print(f"-----|-----|-----")
+        print("-----|-----|-----")
 
 
 def is_not_vaid_input(user_input):
@@ -24,39 +23,91 @@ def update_board(user_id, user_input_position):
 
 def check_for_game(user_id):
     user_mark = user_marking[user_id]
+
     for row in board:
         if row.count(user_mark) == 3:
             return True
+
+    board_transpose = [[row[i] for row in board] for i in range(0, 3)]
+    for row in board_transpose:
+        if row.count(user_mark) == 3:
+            return True
+
+    if [board[i][i] for i in range(0, 3)].count(user_mark) == 3:
+        return True
+    elif [board[x][y] for x, y in zip(range(0, 3), range(0, 3)[::-1])].count(user_mark) == 3:
+        return True
+
     return False
-    # 3 in a row
-    # 3 in a coloumn
-    # 3 diagnoloy-1
-    # 3 diagnoloy-2
+
+
+def is_valid_position(user_input):
+    try:
+        val = int(user_input)
+        if val > 9 or val < 1:
+            return (False, "value should be between 1 -> 9")
+        elif user_input in selected_position_list:
+            print_board()
+            return (False, f" '{user_input}'  already taken")
+        return (True, "")
+    except ValueError:
+        return (False, f" '{user_input}'  is not a number, legal values are 1 -> 9 ")
 
 
 def start_tic_tac_toe():
-    index = 0
+    init()
+    move_index = 0
     while True:
-        if index % 2 == 0:
-            user_id = 1
-        else:
-            user_id = 2
-        user_input_position = input(f"user{user_id} : choose your position from keypad : ")
+        if move_index == 9:
+            print("----------------game DRAW------------------")
+            break
+
+        user_id = get_user_id(move_index)
+        user_input_position = get_valid_position(user_id)
         selected_position_list.append(user_input_position)
         update_board(user_id, user_input_position)
+
         if check_for_game(user_id):
-            print(f"Hurry Congrats User :{user_id} won")
+            print(f"\n\n############  Hurry Congrats User {user_id} won ....!!!!!!  ################\n")
             break
         else:
-            index += 1
+            move_index += 1
+    return input("would you like to play a new game(y/n) : ")
+
+
+def get_valid_position(user_id):
+    user_input_position = input(f"user{user_id} : choose your position from keypad : ")
+    t = is_valid_position(user_input_position)
+
+    while not t[0]:
+        print(f"!!!invalid choice!!! \n cause : {t[1]}")
+        user_input_position = input(f"user{user_id} : please choose a valid position from keypad : ")
+        t = is_valid_position(user_input_position);
+
+    return user_input_position
+
+
+def get_user_id(index):
+    if index % 2 == 0:
+        user_id = 1
+    else:
+        user_id = 2
+    return user_id
+
+
+def init():
+    global board
+    board = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']]
+    selected_position_list.clear()
 
 
 def start_game():
-    user_input = input("welcome to  the tic tac toe : ")
-    if user_input.lower() == 'yes':
-        start_tic_tac_toe()
+    print("**** welcome to  the tic tac toe ****")
+    user_input = start_tic_tac_toe()
+    while user_input.lower() == 'y':
+        user_input = start_tic_tac_toe()
     else:
-        print("thank you for your time")
+        print("Thank you for your time")
 
 
 start_game()
