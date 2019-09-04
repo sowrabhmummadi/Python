a459e726b2e34e169dd15b5e8c84bfb07d391d3e
sowrabhmumadi <sowrabhmummadi@gmail.com>
2019-09-04 14:43:29 +0200

    BlackJack: created a Deck

--- black_jack.py
+++ black_jack.py
@@ -1,28 +1,38 @@
 import random
 
 
+class Card:
+
+    def __init__(self, name, value):
+        self.name = name
+        self.value = value
+
+    def print_card(self):
+        print('-----')
+        print(f'|{self.name[0]}-{self.value}|')
+        print('-----')
+
+
 class Deck:
     cards = []
 
     def __init__(self):
         self.fill_cards()
-        self.shuffel_deck()
+        self.shuffle_deck()
 
     def fill_cards(self):
-        [[self.cards.append(f"{card_type} : {card_value}") for card_value in range(1, 13)] for
+        [[self.cards.append(Card(card_type, card_value)) for card_value in range(1, 13)] for
          card_type in ['Heart', 'Diamond', 'Spade', 'Club']]
 
-    def shuffel_deck(self):
+    def shuffle_deck(self):
         random.shuffle(self.cards)
 
     def print_deck(self):
-        print(self.cards)
+        for card in self.cards:
+            card.print_card()
 
-    class Card:
+    def get_card(self):
+        return self.cards.pop()
 
-        def __init__(self, name, value):
-            self.name = name
-            self.value = value
-
-
-Deck().print_deck()
+    def get_length(self):
+        return len(self.cards)
