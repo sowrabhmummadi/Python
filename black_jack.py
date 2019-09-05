89befb072344463bfa441a4f2f97b018825dfe35
sowrabhmumadi <sowrabhmummadi@gmail.com>
2019-09-05 15:01:51 +0200

    BlackJack: working game without Ace

--- black_jack.py
+++ black_jack.py
@@ -14,8 +14,20 @@ class Player:
     def get_cards(self):
         return self.cards
 
-    def is_dealer(self):
-        return self.is_dealer;
+    def is_dealers(self):
+        return self.is_dealer
+
+    def get_bank_roll(self):
+        return self.bankroll
+
+    def get_name(self):
+        if self.is_dealer:
+            return "Dealer"
+        else:
+            return "Player"
+
+    def get_hand_value(self):
+        return sum(map(lambda x: x.get_value(), self.cards))
 
 
 class Card:
@@ -25,7 +37,10 @@ class Card:
         self.name = name
 
     def print_card(self):
-        print(f'{self.ctype} : {self.value} : {self.name}')
+        return f'{self.value} : {self.ctype} : {self.name}'
+
+    def get_value(self):
+        return self.value
 
 
 class Deck:
@@ -41,7 +56,7 @@ class Deck:
                 self.cards.append(Card(card_type, card_value))
             for card_name in ['Jack', 'Queens', 'King']:
                 self.cards.append(Card(card_type, 10, card_name))
-                self.cards.append(Card(card_type, 1, 'Ace'))
+            # self.cards.append(Card(card_type, 1, 'Ace'))
 
     def shuffle_deck(self):
         random.shuffle(self.cards)
@@ -65,27 +80,63 @@ class BlackJack:
         self.deck = Deck()
 
     def start(self):
-        is_game_ended = False
+        # bet = input(f"please specify your bet(Balance:{self.player.get_bank_roll()}):")
         self.player.add_card(self.deck.get_card())
         self.dealer.add_card(self.deck.get_card())
         self.player.add_card(self.deck.get_card())
         self.dealer.add_card(self.deck.get_card())
-        while not is_game_ended:
-            self.print_cards()
-            break
+        is_game_over = False
 
-    def deal_initial_cards(self):
-        Player.addCaself.deck.get_card()
+        # players turn
+        while True:
+            player_hand_sum = self.player.get_hand_value()
+            if player_hand_sum > 21:
+                self.print_cards()
+                print("~$$$$ player bust $$$$~")
+                is_game_over = True
+                break
+            self.print_cards()
+            print("1.Hit 2.Stand")
+            option = input("Choose your option: ")
+            if option == '1':
+                self.hit(self.player)
+            else:
+                print("player stands...")
+                break
+
+        if not is_game_over:
+            print("Dealers turn")
+            while True:
+                dealer_hand_sum = self.dealer.get_hand_value()
+                if dealer_hand_sum > player_hand_sum or dealer_hand_sum > 17:
+                    break
+                self.hit(self.dealer)
+            self.print_cards()
+            if dealer_hand_sum > 21:
+                print("~$$$$ dealer bust ~$$$$")
+            elif player_hand_sum > dealer_hand_sum:
+                print("~$$$$ player won ~$$$$")
+            elif dealer_hand_sum > player_hand_sum:
+                print("~$$$$ players loose ~$$$$")
+            else:
+                print("~$$$$ Tied ~$$$$")
 
     def print_cards(self):
         for p in [self.player, self.dealer]:
-            if p.is_dealer
-                print('Dealers cards: ', "")
+            if p.is_dealers():
+                print(f'Dealers cards(sum: {p.get_hand_value()}): ', end="")
             else:
-                print('players cards: ', "")
+                print(f'players cards(sum: {p.get_hand_value()}): ', end="")
             for card in p.get_cards():
-                print(f'{card.print_card()} |', "")
+                print(f'{card.print_card()} |', end="")
             print('')
+
+    def hit(self, player):
+        print(f"{player.get_name()} choose to Hit")
+        player.add_card(self.deck.get_card())
+
+    def stand(self, player):
+        print(f"{player.get_name()} choose to Stand")
 
 
 BlackJack().start()
