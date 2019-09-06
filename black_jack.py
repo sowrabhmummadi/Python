888c7c82d56e89519bb30a3fc8591fb25db3f8cd
sowrabhmummadi <sowrabhmummadi@gmail.com>
2019-09-07 00:11:05 +0200

    Black jack: improved output

--- black_jack.py
+++ black_jack.py
@@ -111,21 +111,18 @@ class BlackJack:
     def start_game(self):
         # bet = input(f"please specify your bet(Balance:{self.player.get_bank_roll()}):")
         self.deal_initial_cards()
-
-        self.player.print_cards()
-        self.dealer.print_cards()
         # players turn
-        self.print_template.set_print_fun(self.print_full_cards)
+        self.print_template.set_print_fun(self.print_cards)
         self.print_template.set_turn("players turn")
         is_game_over, player_hand_sum = self.players_turn()
         if not is_game_over:
-            self.print_template.set_turn("Dealer turn")
             is_game_over, dealer_hand_sum = self.dealers_turn(player_hand_sum)
         if not is_game_over:
             self.provide_verdict(dealer_hand_sum, player_hand_sum)
 
     def provide_verdict(self, dealer_hand_sum, player_hand_sum):
         ##self.print_full_cards()
+        self.print_template.set_print_fun(self.print_full_cards)
         if player_hand_sum > dealer_hand_sum:
             self.print_template.set_verdict("~$$$$ player won $$$$~")
         elif dealer_hand_sum > player_hand_sum:
@@ -136,7 +133,7 @@ class BlackJack:
     def dealers_turn(self, player_hand_sum):
         is_game_over = False
         self.print_template.set_turn("Dealers turn")
-        self.print_template.set_Status("")
+        self.print_template.set_status("")
         while True:
             dealer_hand_sum = self.dealer.get_hand_value()
             if dealer_hand_sum > player_hand_sum or dealer_hand_sum > 17:
@@ -144,7 +141,8 @@ class BlackJack:
             self.hit(self.dealer)
         if dealer_hand_sum > blackjack_limit:
             ##self.print_full_cards()
-            self.print_template.set_verdict("~$$$$ dealer bust ~$$$$");
+            self.print_template.set_print_fun(self.print_full_cards)
+            self.print_template.set_verdict("~$$$$ dealer bust ~$$$$")
             is_game_over = True
         return is_game_over, self.dealer.get_hand_value()
 
@@ -157,19 +155,15 @@ class BlackJack:
                 self.hit(self.player)
                 player_hand_sum = self.player.get_hand_value()
                 if player_hand_sum > blackjack_limit:
-                    ## self.print_full_cards()
-                    self.print_template.set_verdict("1.Hit 2.Stand")("~$$$$ player bust $$$$~")
+                    self.print_template.set_print_fun(self.print_full_cards)
+                    self.print_template.set_verdict("~$$$$ player bust $$$$~")
                     is_game_over = True
                     break
-                else:
-                    self.player.print_cards()
-                    self.dealer.print_cards()
-
             elif option == '2':
-                self.print_template.set_Status(f"{self.player} choose to Stand")
+                self.print_template.set_status(f"{self.player} choose to Stand")
                 break
             else:
-                self.print_template.set_Status("please choose a valid option..")
+                self.print_template.set_status("please choose a valid option..")
         return is_game_over, self.player.get_hand_value()
 
     def deal_initial_cards(self):
@@ -180,14 +174,18 @@ class BlackJack:
 
     def hit(self, player):
         player.add_card(self.deck.deal_card())
-        self.print_template.set_Status(f"{player} choose to Hit")
+        self.print_template.set_status(f"{player} choose to Hit")
 
     def print_full_cards(self):
         self.player.print_full_cards()
         self.dealer.print_full_cards()
 
+    def print_cards(self):
+        self.player.print_cards()
+        self.dealer.print_cards()
+
     def stand(self, player):
-        self.print_template.set_Status(f"{player} choose to Stand")
+        self.print_template.set_status(f"{player} choose to Stand")
 
 
 def start_game():
@@ -210,36 +208,42 @@ class PrintTemplate:
         self.fun = None
         self.inpt = None
         self.verdict = None
+        self.history = []
 
     def update_print(self):
         os.system('cls||clear')
         print(f'Turn: {self.turn}')
         print(f'Status: {self.status}')
+        print(f'History: {self.history}')
         self.fun()
-        if self.verdict != None:
+        if self.verdict is not None:
             print(f'Verdict: {self.verdict}')
-        elif self.inpt != None:
+        elif self.inpt is not None:
             user_input = input(f'choose your option ({self.inpt}): ')
             self.inpt = None
             return user_input
 
     def set_turn(self, turn):
         self.turn = turn
+        self.history.append(f"turn: {turn}")
         self.update_print()
 
-    def set_Status(self, status):
+    def set_status(self, status):
         self.status = status
+        if status != "":
+            self.history.append(f"status: {status}")
         self.update_print()
 
     def set_print_fun(self, fun):
         self.fun = fun
-        self.update_print()
 
     def set_input(self, inpt):
         self.inpt = inpt
+        self.history.append(f"choice given: {inpt}")
         return self.update_print()
 
     def set_verdict(self, verdict):
+        self.history.append(f"verdict: {verdict}")
         self.verdict = verdict
         return self.update_print()
 
